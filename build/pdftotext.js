4c56fa5128f7aeecaa8017ecca99fffebd445301
Liran Tal <liran.tal@gmail.com>
2016-09-14 20:10:12 +0000

    refactored code to support ES6 classes

--- build/pdftotext.js
+++ build/pdftotext.js
@@ -2,56 +2,72 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 
-function pdftotext(filename, options) {
-  this.options = options || {};
-  //quote filename
-  this.options.additional = ['"' + filename + '"'];
+var pdftotext = function () {
+  function pdftotext(filename, options) {
+    _classCallCheck(this, pdftotext);
 
-  pdftotext.prototype.add_options = function (optionArray) {
-    var _this = this;
+    this.options = options || {};
+    this.options.additional = ['"' + filename + '"'];
+  }
 
-    if (_typeof(optionArray.length) !== undefined) {
-      optionArray.forEach(function (el) {
-        if (el.indexOf(' ') > 0) {
-          var values = el.split(' ');
-          _this.options.additional.push(values[0], values[1]);
-        } else {
-          _this.options.additional.push(el);
-        }
-      });
+  _createClass(pdftotext, [{
+    key: 'add_options',
+    value: function add_options(optionArray) {
+      var _this = this;
+
+      if (_typeof(optionArray.length) !== undefined) {
+        optionArray.forEach(function (el) {
+          if (el.indexOf(' ') > 0) {
+            var values = el.split(' ');
+            _this.options.additional.push(values[0], values[1]);
+          } else {
+            _this.options.additional.push(el);
+          }
+        });
+      }
+      return this;
     }
-    return this;
-  };
+  }, {
+    key: 'getTextSync',
+    value: function getTextSync() {
+      // '-' stdout the output
+      this.add_options(['-']);
+      return execSync('pdftotext ' + this.options.additional.join(' '));
+    }
+  }, {
+    key: 'getText',
+    value: function getText(cb) {
+      // '-' stdout the output
+      this.add_options(['-']);
+      exec('pdftotext ' + this.options.additional.join(' '), cb);
+    }
+  }, {
+    key: 'error',
+    value: function error(callback) {
+      this.options.error = callback;
+      return this;
+    }
+  }, {
+    key: 'success',
+    value: function success(callback) {
+      this.options.success = callback;
+      return this;
+    }
+  }]);
 
-  pdftotext.prototype.getTextSync = function () {
-    var self = this;
-    // '-' stdout the output
-    self.add_options(['-']);
-    return execSync('pdftotext ' + self.options.additional.join(' '));
-  };
-
-  pdftotext.prototype.getText = function (cb) {
-    var self = this;
-    // '-' stdout the output
-    self.add_options(['-']);
-    exec('pdftotext ' + self.options.additional.join(' '), cb);
-  };
-
-  pdftotext.prototype.error = function (callback) {
-    this.options.error = callback;
-    return this;
-  };
-
-  pdftotext.prototype.success = function (callback) {
-    this.options.success = callback;
-    return this;
-  };
-}
+  return pdftotext;
+}();
 
 // module exports
+
+
 exports = module.exports = function (filename, args) {
   return new pdftotext(filename, args);
 };
