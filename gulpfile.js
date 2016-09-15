7b3721a3b5665290f147da7a2c75e28b34f3444f
Liran Tal <liran.tal@gmail.com>
2016-09-15 16:30:06 +0000

    removing not required

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 var gulp = require('gulp');
-var print = require('gulp-print');
 var babel = require('gulp-babel');
 
 gulp.task('compile', function() {
     return gulp.src('lib/*.js')
-        .pipe(print())
         .pipe(babel({ presets: ['es2015'] }))
         .pipe(gulp.dest('build'));
 });
